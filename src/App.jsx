1883e4cc53779c7eba891112fe936ea349230566
Leslie <wilson.leslie2021@gmail.com>
2025-11-11 19:13:24 -0500

    Adding files

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import './App.css'
+
+
+//* 1st Input *//
+
+function App(props) {
+  return (
+    <div className="todoapp stack-large">
+      <h1>Create Todo List</h1>
+      <input type="text" id="first-todo-input" name="text" />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+
+      {/* End 1st Input */}
+
+
+      {/* Create Mockup */}
+
+      <div>
+        <input id="todo-0" type="checkbox" />
+        <label className="todo-label" htmlFor="todo-0">
+          Create Mockup
+        </label>
+      </div>
+
+      <div className="btn-group">
+        <button type="button" className="btn">
+          Edit
+        </button>
+        <button type="button" className="btn btn__danger">
+          Delete
+        </button>
+      </div>
+
+      {/* End Mockup */}
+
+
+      {/* Create Static */}
+
+      <div className="c-cb">
+        <input id="todo-1" type="checkbox" />
+        <label className="todo-label" htmlFor="todo-1">
+          Create Static Layout
+        </label>
+      </div>
+      <div className="btn-group">
+        <button type="button" className="btn">
+          Edit
+        </button>
+        <button type="button" className="btn btn__danger">
+          Delete
+        </button>
+      </div>
+
+      {/* End Static */}
+
+
+      {/* 2nd Input */}
+
+      <input id="todo-2" type="checkbox" />
+      <input type="text" id="second-todo-input" name="text" />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+
+      {/* 2nd Input */}
+      
+
+    </div>
+  );
+}
+
+export default App;
